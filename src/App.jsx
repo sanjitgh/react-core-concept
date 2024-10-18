db2a90d4a61c9692954c89883bcd179b20be5652
Sanjit <sanjitkumarghosh0@gmail.com>
2024-10-19 02:37:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  function handelClick() {
+    alert("button clicked");
+  }
+  const handelClick2 = () => alert("button clicked 2");
+  const handelClick3 = (num) => {
+    alert(num + 5);
+  };
+  return (
+    <>
+      <h1>React</h1>
+      <button onClick={handelClick}>Click</button>
+      <button onClick={handelClick2}>Click2</button>
+      <button onClick={() => handelClick3(2)}>click3</button>
+    </>
+  );
+}
+
+export default App;
